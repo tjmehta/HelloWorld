55502a9d4b36849352d978dee4167649d6aedc8d
Tejesh Mehta <tejesh.mehta@gmail.com>
2014-10-01 00:04:02 -0700

    use and require process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
+if (!process.env.PORT) {
+  throw new Error('process.env.PORT is required!');
+}
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!?!?!\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+  res.end('Hello World!\n');
+}).listen(process.env.PORT);
+console.log('Server running at http://127.0.0.1:'+process.env.PORT+'/');
