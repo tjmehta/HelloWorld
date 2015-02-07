fb5eecbcb4fe344986822817bf6fe0546701ae73
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-02-06 16:12:15 -0800

    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ if (!process.env.PORT) {
 }
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World!\n');
 }).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:'+process.env.PORT+'/');
