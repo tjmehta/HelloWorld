e3bfbe9beb93cd5f98476dc10dc408208fc33b24
Tejesh Mehta <tejesh.mehta@gmail.com>
2015-06-11 18:27:37 -0700

    sigint/sigterm testing

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ http.createServer(function (req, res) {
   res.end('Hello World!\n');
 }).listen(process.env.PORT);
 console.log('Server running at http://127.0.0.1:'+process.env.PORT+'/');
+
+process.on('SIGINT', console.log.bind(console, 'SIGINT'));
+process.on('SIGINT', console.log.bind(console, 'SIGINT'));
+process.on('SIGINT', console.log.bind(console, 'SIGINT'));
+process.on('SIGINT', console.log.bind(console, 'SIGINT'));
+process.on('SIGINT', console.log.bind(console, 'SIGINT'));
+process.on('SIGTERM', console.log.bind(console, 'SIGTERM'));
+process.on('SIGTERM', console.log.bind(console, 'SIGTERM'));
+process.on('SIGTERM', console.log.bind(console, 'SIGTERM'));
+process.on('SIGTERM', console.log.bind(console, 'SIGTERM'));
+process.on('SIGTERM', console.log.bind(console, 'SIGTERM'));
